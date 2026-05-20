9fa020c90de8fd94005fc10a56e5a82c3176c7be
Rapha Ueda <rueda@iportsolutions.com.br>
2026-05-20 10:19:19 -0300

    feat: update assets and scripts for improved performance and consistency
    - Added new JavaScript files for home and privacy pages.
    - Removed outdated privacy script and preload helper.
    - Updated preload helper with optimized code.
    - Added new icon image and updated references in HTML files.
    - Replaced SVG icon with PNG in multiple locations.
    - Updated package dependencies for nanoid and postcss.
    - Added app-ads.txt for ad configuration.

---
+++ dist/assets/Privacy-PYALMVfD.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Privacy.vue_vue_type_script_setup_true_lang-D7lKw0tN.js";import"./preload-helper-ckwbz45p.js";import"./runtime-dom.esm-bundler-v1CrdU6e.js";export{o as default};
--- dist/assets/jogatina-BFcAdtZ_.js
+++
@@ -1,1 +0,0 @@
-import{l as m}from"./runtime-dom.esm-bundler-v1CrdU6e.js";import{_ as o}from"./Jogatina.vue_vue_type_style_index_0_lang-CRPnp9eZ.js";m(o).mount("#app-home");
